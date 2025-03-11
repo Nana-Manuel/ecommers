17aa19aed153828a250e83f361daf97b156e4156
Nana MAnuel <emmanuelagbosu29@gmail.com>
2025-03-11 15:17:30 +0000

    added the bottom navigation

--- app/src/main/java/com/example/e_commers/ui/theme/Theme.kt
+++ app/src/main/java/com/example/e_commers/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.e_commers.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Secondary_Color,
+    secondary = Secondary_Gray,
+    tertiary = Secondary80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Primary_Color,
+    secondary = Primary_Gray,
+    tertiary = Primary80
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
